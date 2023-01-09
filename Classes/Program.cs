e03eeaf492a108427be525e3c134f837e428872f
ross dibartolo <dibartoloross@gmail.com>
2023-01-09 12:47:59 -0500

    initial commit

---
+++ Classes/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Classes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var wrangler = new Car();
+            wrangler.Make = "Jeep";
+            wrangler.Model = "Wrangler";
+            wrangler.Year = 2023;
+
+            wrangler.MyCar();
+        }
+    }
+}
